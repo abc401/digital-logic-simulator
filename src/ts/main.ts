c8e705cc6612eb84467aa235f971b884f1a34289
Awab Ahmed <koibhinheen@gmail.com>
2023-11-05 01:14:50 -0700

    gates now respond to input changes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,38 +1,81 @@
 class Wire {
-  from: ProducerPin;
-  to: ConsumerPin;
+  producerCircuit: Circuit;
+  producerPinNumber: number;
+  consumerCircuit: Circuit;
+  consumerPinNumber: number;
 
-  constructor(from: ProducerPin, to: ConsumerPin) {
-    this.from = from;
-    this.to = to;
+  constructor(
+    from: Circuit,
+    fromPinNumber: number,
+    to: Circuit,
+    toPinNumber: number
+  ) {
+    this.producerCircuit = from;
+    this.producerPinNumber = fromPinNumber;
+    from.producerPins[fromPinNumber].wires.push(this);
+
+    this.consumerCircuit = to;
+    this.consumerPinNumber = toPinNumber;
   }
 
   propogateValue(value: boolean) {
-    this.to.consumeValue(value);
+    this.consumerCircuit.consumerPins[this.consumerPinNumber].consumeValue(
+      value
+    );
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    const fromX =
+      this.producerCircuit.producerPins[this.producerPinNumber].pos_x;
+    const fromY =
+      this.producerCircuit.producerPins[this.producerPinNumber].pos_y;
+    const toX = this.consumerCircuit.consumerPins[this.consumerPinNumber].pos_x;
+    const toY = this.consumerCircuit.consumerPins[this.consumerPinNumber].pos_y;
+
+    if (this.producerCircuit.producerPins[this.producerPinNumber].value) {
+      ctx.strokeStyle = "blue";
+    } else {
+      ctx.strokeStyle = "black";
+    }
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    ctx.closePath();
+    ctx.stroke();
   }
 }
 
 class ConsumerPin {
-  wire: Wire | undefined;
+  static radius = 10;
+  circuit: Circuit;
   value: boolean;
+  pos_x: number;
+  pos_y: number;
 
-  constructor(wire: Wire | undefined = undefined) {
-    this.wire = wire;
+  constructor(circuit: Circuit, pos_x: number, pos_y: number) {
+    this.circuit = circuit;
     this.value = false;
+    this.pos_x = pos_x;
+    this.pos_y = pos_y;
   }
 
   consumeValue(value: boolean) {
     this.value = value;
+    this.circuit.func(this.circuit);
   }
-  draw(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number) {
+
+  draw(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.arc(this.pos_x, this.pos_y, ConsumerPin.radius, 0, 2 * Math.PI);
     if (this.value) {
       ctx.fillStyle = "red";
       ctx.fill();
     } else {
       ctx.lineWidth = 1;
+      ctx.fillStyle = "white";
+      ctx.fill();
       ctx.strokeStyle = "red";
       ctx.stroke();
     }
@@ -40,30 +83,37 @@ class ConsumerPin {
 }
 
 class ProducerPin {
-  wire: Wire | undefined;
+  static radius = 10;
+  wires: Wire[];
   value: boolean;
+  pos_x: number;
+  pos_y: number;
 
-  constructor(wire: Wire | undefined = undefined) {
-    this.wire = wire;
-    this.value = false;
+  constructor(pos_x: number, pos_y: number, value: boolean = false) {
+    this.wires = [];
+    this.value = value;
+    this.pos_x = pos_x;
+    this.pos_y = pos_y;
   }
 
   setValue(value: boolean) {
     this.value = value;
-    if (this.wire) {
-      this.wire.propogateValue(value);
+    for (let i = 0; i < this.wires.length; i++) {
+      this.wires[i].propogateValue(value);
     }
   }
 
-  draw(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number) {
+  draw(ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.arc(this.pos_x, this.pos_y, ConsumerPin.radius, 0, 2 * Math.PI);
     if (this.value) {
       ctx.fillStyle = "red";
       ctx.fill();
     } else {
       ctx.lineWidth = 1;
+      ctx.fillStyle = "white";
+      ctx.fill();
       ctx.strokeStyle = "red";
       ctx.stroke();
     }
@@ -92,6 +142,9 @@ class Circuit {
     runMethod: CircuitRunMethod = CircuitRunMethod.OnInputChange,
     func: undefined | ((self: Circuit) => void) = undefined
   ) {
+    this.pos_x = pos_x;
+    this.pos_y = pos_y;
+
     if (nConsumerPins % 1 !== 0) {
       throw Error(
         `Expected nConsumerPins to be integer but got: ${nConsumerPins}`
@@ -105,16 +158,21 @@ class Circuit {
 
     this.consumerPins = Array(nConsumerPins);
     for (let i = 0; i < this.consumerPins.length; i++) {
-      this.consumerPins[i] = new ConsumerPin();
+      this.consumerPins[i] = new ConsumerPin(
+        this,
+        this.pos_x,
+        this.pos_y + i * 70
+      );
     }
 
     this.producerPins = Array(nProducerPins);
     for (let i = 0; i < this.producerPins.length; i++) {
-      this.producerPins[i] = new ProducerPin();
+      this.producerPins[i] = new ProducerPin(
+        this.pos_x + Circuit.width,
+        this.pos_y + i * 70
+      );
     }
-    this.func = func || ((self) => {});
-    this.pos_x = pos_x;
-    this.pos_y = pos_y;
+    this.func = func || ((_self) => {});
 
     if (runMethod === CircuitRunMethod.Interval && this.func != null) {
       setInterval(() => {
@@ -124,6 +182,12 @@ class Circuit {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < this.consumerPins.length; i++) {
+      this.consumerPins[i].draw(ctx);
+    }
+    for (let i = 0; i < this.producerPins.length; i++) {
+      this.producerPins[i].draw(ctx);
+    }
     ctx.fillStyle = "cyan";
     ctx.fillRect(
       this.pos_x,
@@ -133,35 +197,52 @@ class Circuit {
         ? this.consumerPins.length * 70
         : this.producerPins.length * 70
     );
-
-    for (let i = 0; i < this.consumerPins.length; i++) {
-      this.consumerPins[i].draw(ctx, this.pos_x, this.pos_y + i * 70, 10);
-    }
-    for (let i = 0; i < this.producerPins.length; i++) {
-      this.producerPins[i].draw(
-        ctx,
-        this.pos_x + Circuit.width,
-        this.pos_y + i * 70,
-        10
-      );
-    }
   }
 }
 
-const Clock = new Circuit(0, 1, 30, 30, CircuitRunMethod.Interval, (self) => {
+const Clock1 = new Circuit(0, 1, 30, 30, CircuitRunMethod.Interval, (self) => {
   self.producerPins[0].setValue(!self.producerPins[0].value);
 });
-const OutputDisplay = new Circuit(1, 0, 200, 30);
 
-const wire = new Wire(Clock.producerPins[0], OutputDisplay.consumerPins[0]);
+const Or = new Circuit(
+  2,
+  1,
+  300,
+  30,
+  CircuitRunMethod.OnInputChange,
+  (self) => {
+    self.producerPins[0].setValue(
+      self.consumerPins[0].value || self.consumerPins[1].value
+    );
+  }
+);
 
-Clock.producerPins[0].wire = wire;
-OutputDisplay.consumerPins[0].wire = wire;
+const Not = new Circuit(
+  1,
+  1,
+  150,
+  100,
+  CircuitRunMethod.OnInputChange,
+  (self) => {
+    self.producerPins[0].setValue(!self.consumerPins[0].value);
+  }
+);
+
+const circuits = [Clock1, Or, Not];
+const wires = [
+  new Wire(Clock1, 0, Or, 0),
+  new Wire(Clock1, 0, Not, 0),
+  new Wire(Not, 0, Or, 1),
+];
 
 function draw(ctx: CanvasRenderingContext2D) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  Clock.draw(ctx);
-  OutputDisplay.draw(ctx);
+  for (let i = 0; i < wires.length; i++) {
+    wires[i].draw(ctx);
+  }
+  for (let i = 0; i < circuits.length; i++) {
+    circuits[i].draw(ctx);
+  }
   console.log("draw");
 }
 
